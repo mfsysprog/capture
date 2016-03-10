afac036ed016048a4bbe580e1d76fe2dc330476c
erik <eaw.janssen@chello.nl>
2016-03-10 19:10:16 +0100

    initial commit

---
+++ src/BBBTest.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : BBBTest.cpp
+// Author      : Erik Janssen
+// Version     :
+// Copyright   : Copyright or left or upside down
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+/*
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+*/
